96f9ed85af318fd9eb5a775ee28e874fddedc26f
vishal637yadav <vishal637yadav@gmail.com>
2023-02-05 02:32:32 +0530

    Initial Commit.

---
+++ src/main/java/ds/arrayslist/DemoArrayListMain.java
@@ -0,0 +1,14 @@
+package ds.arrayslist;
+
+import java.util.ArrayList;
+
+public class DemoArrayListMain {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Me added");
+        System.out.println("list :->"+list);
+
+    }
+}
